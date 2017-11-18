2d4cfab3f8d3a9ab38858f3ead0ce12312119bd1
Emilian Peev <emilian.peev@gmail.com>
2017-11-18 17:37:45 +0000

    Verify computed results
    The test bench should deserialize, check and verify the computed
    results received on the serial line.

--- top.v
+++ top.v
@@ -16,6 +16,12 @@
 
 `include "cpu.v"
 
-module top(input clk, output uart_tx_line);
-  cpu node(clk, uart_tx_line);
+module top(
+  input clk,
+  output uart_tx_line);
+
+  cpu node(
+    .clk(clk),
+    .uart_tx_wire(uart_tx_line));
+
 endmodule
--- top_test.v
+++ top_test.v
@@ -19,16 +19,80 @@
 
 module top_test;
 
-  reg clk = 0;
-  wire uart_tx;
-  always #2 clk = !clk;
-  initial
-    # 2500 $finish;
+  localparam WIDTH = 8;
+  localparam UART_WIDTH = $clog2(WIDTH);
+  localparam OUTPUT_CNT = 12;
 
-  top t(clk, uart_tx);
+  reg clk = 1;
+  reg uart_clk = 0;
+  reg receiving = 0;
+  reg display = 0;
+  reg [UART_WIDTH-1 : 0] serial_cnt = 0;
+  reg [WIDTH-1 : 0] serial_data;
+  reg [WIDTH-1 : 0] expected_output [OUTPUT_CNT-1 : 0];
+  wire uart_tx;
+
+  reg [WIDTH-1 : 0] i, j, k, l;
+  initial begin
+    j = 1;
+    k = 1;
+    l = 0;
+    for (i = 0; i < OUTPUT_CNT; i = i + 1) begin
+      expected_output[i] = k;
+      l = k;
+      k = k + j;
+      j = l;
+    end
+    i = 0;
+  end
+
+  always #2 clk = !clk;
+  always #4 uart_clk = !uart_clk;
+
+  top t(
+    .clk(clk),
+    .uart_tx_line(uart_tx));
 
   initial begin
     $dumpfile("top_test.vcd");
     $dumpvars;
   end
+
+  always @ (posedge uart_clk) begin
+    if (receiving) begin
+
+      if (serial_cnt == WIDTH - 1 ) begin
+        receiving <= 0;
+        display <= 1;
+      end
+
+      serial_data[serial_cnt] <= uart_tx;
+      serial_cnt <= serial_cnt + 1;
+
+    end else if (display) begin
+
+      if (i >= OUTPUT_CNT) begin
+        $display("Test passed, computed results match the expected output!\n");
+        $finish;
+      end
+
+      if (serial_data != expected_output[i]) begin
+        $display("Test failed!\n");
+        $display("Serial output:%d doesn't match expected_output[%d]:%d\n",
+          serial_data, i, expected_output[i]);
+        $finish;
+      end
+
+      i <= i + 1;
+      display <= 0;
+
+    end else begin
+
+      if (uart_tx == 0) begin
+        receiving <= 1;
+      end
+
+    end
+  end
+
 endmodule
